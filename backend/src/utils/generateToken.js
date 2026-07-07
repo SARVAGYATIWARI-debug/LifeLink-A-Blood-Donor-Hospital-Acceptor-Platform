01364b6e66df8705970f7f8a3d64c4134d71d1a7
SARVAGYATIWARI-debug <sarvagyatiwariaiml@gmail.com>
2026-07-07 16:55:29 +0530

    Fix cross-site cookie sameSite policy for production

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId, role) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-    sameSite: 'strict',
+    sameSite: process.env.NODE_ENV !== 'development' ? 'none' : 'strict',
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
